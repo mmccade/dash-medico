03cc13f17698494cc2aff589fe31859dc3468baf
madeira <souzasoaresgabrie9@gmail.com>
2026-06-29 01:22:25 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173, host: true },
+  optimizeDeps: {
+    include: ["html2pdf.js"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "html2pdf": ["html2pdf.js"],
+        },
+      },
+    },
+  },
 });
